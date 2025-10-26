ee72039dcb3957bea6ca67cd7cdf5089602f56ac
西藤実 <saitominoru@saitouminorunoMacBook-Pro.local>
2025-10-26 18:40:53 +0900

    2D移動軌跡に明度で高さを表現できる機能を追加。軽微なバグの修正。

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // キャッシュ名をユニークにするためにバージョンを付けます。ファイルを更新した際には、このバージョン番号を上げてください。
-const CACHE_NAME = 'mp-pwa-cache-20251021-1'; 
+const CACHE_NAME = 'mp-pwa-cache-20251026'; 
 const urlsToCache = [
   './',
   './index.html',
